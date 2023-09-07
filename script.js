b10da564fb3ff29abea20c4764fbecc066855c06
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-09-07 15:04:09 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+			let first = -Infinity, second = -Infinity;
+			for (let i = 0; i < arr.length; i++) {
+				if (arr[i] > first) {
+				second = first;
+				first = arr[i];
+				} else if (arr[i] > second && arr[i] < first) {
+				second = arr[i];
+				}
+			}
+			return second;
+		}
+
+		function Main() {
+			var n = prompt("Enter the number of elements");
+			var arr = [];
+			for (var i = 0; i < n; i++) {
+				arr[i] = prompt("Enter element " + (i+1));
+			}
+			alert(secondHighest(arr));
+		}
+		Main();
